570520e599d0ee42fa670294a1fd4e86c0b09ec6
white <530660508@qq.com>
2020-01-02 15:20:18 +0800

    test

--- blog_server/src/main/java/com/blog/service/UserService.java
+++ blog_server/src/main/java/com/blog/service/UserService.java
@@ -1,4 +1,8 @@
 package com.blog.service;
 
+import com.blog.model.UserInfo;
+
 public interface UserService {
+
+    UserInfo getUserById(int id);
 }
--- blog_server/src/main/java/com/blog/service/impl/UserServiceImpl.java
+++ blog_server/src/main/java/com/blog/service/impl/UserServiceImpl.java
@@ -1,9 +1,14 @@
 package com.blog.service.impl;
 
 
+import com.blog.model.UserInfo;
 import com.blog.service.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+    @Override
+    public UserInfo getUserById(int id) {
+        return null;
+    }
 }
