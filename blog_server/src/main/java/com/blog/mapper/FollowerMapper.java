37a5464d0d0ed655ea8c1037cdbffefb13a1f3a7
dxy <872246062@qq.com>
2020-01-13 14:31:05 +0800

    增加新的表

--- blog_server/src/main/java/com/blog/mapper/FollowerMapper.java
+++ blog_server/src/main/java/com/blog/mapper/FollowerMapper.java
@@ -1,10 +1,13 @@
 package com.blog.mapper;
 
+import com.blog.model.Follower;
 import com.blog.model.FollowerKey;
 
 public interface FollowerMapper {
     int deleteByPrimaryKey(FollowerKey key);
 
     int insert(FollowerKey record);
+
+    Follower selectByUid(Integer uid);
 
 }
