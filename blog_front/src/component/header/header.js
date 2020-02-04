51cb0bb372d6c0749b0b338bef8b991b4302066d
white <530660508@qq.com>
2020-02-04 15:52:59 +0800

    加个回首页的logo

--- blog_front/src/component/header/header.js
+++ blog_front/src/component/header/header.js
@@ -6,6 +6,7 @@ import Search from "../search/search";
 export const Nav = () => {
     return (
         <div className="header">
+            <a className="logo" href="/"><img src="image/blog.png" alt="首页"/></a>
             <Search />
             <ToLogin/>
         </div>
