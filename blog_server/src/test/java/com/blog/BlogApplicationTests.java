2a17b3dddc74e6799cdf64736d87de51e5118be1
dxy <872246062@qq.com>
2020-02-10 15:49:12 +0800

    修改部分代码

--- blog_server/src/test/java/com/blog/BlogApplicationTests.java
+++ blog_server/src/test/java/com/blog/BlogApplicationTests.java
@@ -27,21 +27,7 @@ class BlogApplicationTests {
 
     @Test
     void articleTest() {
-//        User user = new User();
-//        user.setUname("aaa");
-//        user.setPassword("123456");
-//        user.setEmail("dxy@1234.com");
-//        user.setBirth(new Date());
-//        user.setGender("M");
-//        userMapper.insert(user);
-//        User u1 = userMapper.selectByEmail("1234@dxy.com");
-        List<Article> list = markService.getMarkedArticles(1,1);
-        for (Article a:list) {
-            System.out.println(a.getArticleTitle());
-        }
 
-
-        System.out.println("--------------");
     }
 
 }
